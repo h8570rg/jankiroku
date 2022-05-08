83386c4bf06f427f263ffe6827b36990f39e06eb
Tetsuo Hirota <54166240+h8570rg@users.noreply.github.com>
2022-05-09 01:12:03 +0900

    fix: css reset

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,19 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import CssBaseline from "@mui/material/CssBaseline";
 import type { AppProps } from "next/app";
 import AuthProvider from "@/context/auth";
 import "../styles/globals.css";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <>
+      <CssBaseline />
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </>
   );
 };
 
