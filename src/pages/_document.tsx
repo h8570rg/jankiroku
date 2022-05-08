10d375a3db7eae09970adb78d687e612e2c31517
Tetsuo Hirota <54166240+h8570rg@users.noreply.github.com>
2022-05-09 00:49:16 +0900

    feat: mui (#15)

---
+++ src/pages/_document.tsx
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+const Document = () => {
+  return (
+    <Html>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default Document;
