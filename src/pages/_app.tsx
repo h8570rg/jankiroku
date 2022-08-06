db015678f3e144c044b545e8385086c3ab1b8c2c
Tetsuo Hirota <54166240+h8570rg@users.noreply.github.com>
2022-08-06 19:29:29 +0900

    fix: layout (#25)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,18 +4,13 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { ThemeProvider } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
-import { NextPage } from "next";
 import type { AppProps } from "next/app";
-import { ReactElement, ReactNode } from "react";
 import BasicLayout from "@/components/layout/basic";
 import AuthProvider from "@/context/auth";
 import ToastProvider from "@/context/toast";
 import { theme } from "@/styles/theme";
+import { NextPageWithLayout } from "@/types";
 import "../styles/globals.css";
-
-type NextPageWithLayout = NextPage & {
-  getLayout?: (page: ReactElement) => ReactNode;
-};
 
 type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -1,21 +1,31 @@
+import Image from "next/image";
 import Link from "next/link";
 import { signInWithGoogle } from "@/apis/client/auth";
 import GoogleIconNormal from "@/public/google/vector/btn_google_light_normal_ios.svg";
+import { NextPageWithLayout } from "@/types";
 
-const Signin = () => {
+const Signin: NextPageWithLayout = () => {
   return (
-    <div>
-      <Link href="/signup" passHref>
-        <a>新規アカウント作成</a>
-      </Link>
-      {/* <Link href="/login/email" passHref>
+    <div className="h- w-full relative">
+      <div className="h-screen w-full relative">
+        <Image src="/bg/mahjong1.jpeg" alt="bg" layout="fill" />
+        <div className="absolute inset-0 backdrop-blur-sm"></div>
+      </div>
+      <div className="absolute top-0">
+        <Link href="/signup" passHref>
+          <a>新規アカウント作成</a>
+        </Link>
+        {/* <Link href="/login/email" passHref>
         <a>メールアドレスでログイン</a>
       </Link> */}
-      <button onClick={signInWithGoogle}>
-        <GoogleIconNormal />
-      </button>
+        <button onClick={signInWithGoogle}>
+          <GoogleIconNormal />
+        </button>
+      </div>
     </div>
   );
 };
+
+Signin.getLayout = (page) => page;
 
 export default Signin;
