0662d75f4e5ada45343c1af9b427a0649cb2c26d
Tetsuo Hirota <54166240+h8570rg@users.noreply.github.com>
2022-08-25 06:11:51 +0900

    fix: font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,3 @@
-import "@fontsource/noto-sans-jp/400.css";
-import "@fontsource/noto-sans-jp/500.css";
-import "@fontsource/noto-sans-jp/700.css";
-import "@fontsource/righteous/400.css";
 import { ThemeProvider } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
 import type { AppProps } from "next/app";
