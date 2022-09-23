f2f8d640a6e036fec92fc721d40c354e7e17a59e
Tetsuo Hirota <54166240+h8570rg@users.noreply.github.com>
2022-09-24 06:43:10 +0900

    refactor: services

--- src/lib/services/user.ts
+++ src/lib/services/user.ts
@@ -2,6 +2,7 @@ import * as userRepo from "~/lib/repositories/user";
 
 export const getUser = (uid: string) => userRepo.fetchUser(uid);
 
+// TODO: 消す
 // export const createUser = (authInfo: AuthInfo) => {
 //   if (isAnonymous(authInfo)) {
 //     const user: User = {
