df150e1c6103dbae9b0890bef7fd1618e6dde278
h8570rg <namao0627@gmail.com>
2023-02-18 01:54:23 +0900

    fix: supabase

--- supabase/migrations/20230215180233_remote_commit.sql
+++
@@ -1,7 +0,0 @@
-alter table "auth"."users" add column "deleted_at" timestamp with time zone;
-
-alter table "auth"."users" alter column "phone" set data type text using "phone"::text;
-
-alter table "auth"."users" alter column "phone_change" set data type text using "phone_change"::text;
-
-
