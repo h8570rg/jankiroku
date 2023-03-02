96028ebc3ec55f2b309389881ae652a868820343
h8570rg <namao0627@gmail.com>
2023-03-03 00:29:13 +0900

    feat: signin

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,32 @@
 import classNames from "classnames";
 import { Metadata } from "next";
+
+import SupabaseListener from "~/components/SupabaseListener";
+import SupabaseProvider from "~/components/SupabaseProvider";
+import { createClient } from "~/lib/utils/supabase-server";
 import "~/styles/globals.css";
+
 import { notoSansJp, righteous, roboto } from "./fonts";
+
+// do not cache this layout
+export const revalidate = 0;
 
 export const metadata: Metadata = {
   title: "Home",
   description: "Welcome to Next.js",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const supabase = createClient();
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
   return (
     <html
       lang="ja"
@@ -22,7 +36,12 @@ export default function RootLayout({
         roboto.variable
       )}
     >
-      <body className="font-sans">{children}</body>
+      <body className="font-sans">
+        <SupabaseProvider>
+          <SupabaseListener serverAccessToken={session?.access_token} />
+          {children}
+        </SupabaseProvider>
+      </body>
     </html>
   );
 }
---
+++ lib/utils/supabase-server.ts
@@ -0,0 +1,12 @@
+import "server-only";
+
+import { createServerComponentSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { headers, cookies } from "next/headers";
+
+import { Database } from "~/lib/database.types";
+
+export const createClient = () =>
+  createServerComponentSupabaseClient<Database>({
+    headers,
+    cookies,
+  });
