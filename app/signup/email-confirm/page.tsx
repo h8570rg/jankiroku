372c1668dc74923788252cee1559c92199d556d5
h8570rg <namao0627@gmail.com>
2023-03-11 16:08:38 +0900

    fix: test

--- app/signup/email-confirm/page.tsx
+++ app/signup/email-confirm/page.tsx
@@ -1,3 +1,23 @@
+"use client";
+
+import { useEffect } from "react";
+
+import { useSupabase } from "~/components/SupabaseProvider";
+
 export default function EmailConfirm() {
+  const { supabase } = useSupabase();
+
+  useEffect(() => {
+    supabase.auth
+      .getSession()
+      .then((session) => {
+        // eslint-disable-next-line no-console
+        console.debug({ session });
+      })
+      .catch((err) => {
+        // eslint-disable-next-line no-console
+        console.debug({ err });
+      });
+  }, [supabase.auth]);
   return "aaa";
 }
