b8881aa85b51b4809dcf6b780041db606b4e95af
h8570rg <namao0627@gmail.com>
2023-03-25 22:10:18 +0900

    fix: redirect

--- app/(auth)/redirect/page.tsx
+++ app/(auth)/redirect/page.tsx
@@ -14,6 +14,7 @@ export default function Redirect() {
     const session = await getSession();
     if (!session) {
       setFailed(true);
+      return;
     }
     router.push("/");
   }, [getSession, router]);
