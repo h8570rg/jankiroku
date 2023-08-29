c43063d7519b35141e802e6f9e518ac2f35cfece
h8570rg <namao0627@gmail.com>
2023-08-30 00:20:04 +0900

    chore: test

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -12,6 +12,9 @@ export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
 
+  // eslint-disable-next-line no-console
+  console.log("code", code);
+
   if (code) {
     const supabase = createSupabaseClient();
     await supabase.auth.exchangeCodeForSession(code);
