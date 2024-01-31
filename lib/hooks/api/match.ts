847f73453efc63e97490c4e36d84ef5d75bfe948
h8570rg <namao0627@gmail.com>
2024-02-01 00:50:39 +0900

    fix: hooks

--- lib/hooks/api/match.ts
+++ lib/hooks/api/match.ts
@@ -1,32 +1,12 @@
 import useSWR from "swr";
 import useSWRMutation from "swr/mutation";
-import { z } from "zod";
 import { Match } from "~/lib/services/match";
-import { CreateMatchPayload, Matches } from "~/lib/services/matches";
 import { get, post } from "~/lib/utils/request";
-import { schemas } from "~/lib/utils/schemas";
 
 export const useMatch = (match: Match) => {
   return useSWR(`/api/matches/${match.id}`, (url) => get<Match>(url), {
     fallbackData: match,
   });
-};
-
-export const useMatches = (initialData: Matches) => {
-  return useSWR("/api/matches", () => get<Matches>("/api/matches"), {
-    fallbackData: initialData,
-  });
-};
-
-export const useMatchCreate = () => {
-  return useSWRMutation(
-    "/api/matches",
-    async (url, { arg }: { arg: MatchCreateSchema }) => {
-      const payload: CreateMatchPayload = arg;
-      const match = await post<{ id: string }>(url, payload); // TODO: Match型にする
-      return match;
-    },
-  );
 };
 
 export const useMatchPlayerAdd = (matchId: string) => {
@@ -38,15 +18,3 @@ export const useMatchPlayerAdd = (matchId: string) => {
     },
   );
 };
-
-export const matchCreateSchema = z.object({
-  calcMethod: schemas.calcMethod,
-  chipRate: schemas.chipRate,
-  crackBoxBonus: schemas.crackBoxBonus,
-  defaultCalcPoints: schemas.defaultCalcPoints,
-  defaultPoints: schemas.defaultPoints,
-  playersCount: schemas.playersCount,
-  rate: schemas.rate,
-  incline: schemas.incline,
-});
-export type MatchCreateSchema = z.infer<typeof matchCreateSchema>;
