ab04f55133f835008e57ee842a6f949e9c152f49
h8570rg <namao0627@gmail.com>
2024-02-24 21:11:07 +0900

    fix: table

--- app/(app)/matches/[matchId]/MatchTable/index.tsx
+++ app/(app)/matches/[matchId]/MatchTable/index.tsx
@@ -6,14 +6,27 @@ import {
   TableBody,
   TableCell,
   TableColumn,
+  TableFooter,
+  TableFooterCell,
+  TableFooterRow,
   TableHeader,
   TableRow,
-  getKeyValue,
 } from "~/components/Table";
 import { Game } from "~/lib/services/game";
 import { Match } from "~/lib/services/match";
 import { AddGameButton } from "./AddGameButton";
 import { AddPlayerButton } from "./AddPlayerButton";
+
+type Column = {
+  id: string;
+  janrecoId: string;
+  name: string;
+  type: "index" | "player" | "empty";
+};
+
+type Row = {
+  [playerId: Column["id"]]: number;
+};
 
 export function MatchTable({ match, games }: { match: Match; games: Game[] }) {
   const { rule, players } = match;
@@ -22,13 +35,7 @@ export function MatchTable({ match, games }: { match: Match; games: Game[] }) {
   const playersShortCount = playersCount - players.length;
   const isPlayersShort = playersShortCount > 0;
 
-  const playerColumns: {
-    id: string;
-    janrecoId: string;
-    name: string;
-    type: "index" | "player" | "empty";
-  }[] = [
-    { id: "index", janrecoId: "", name: "", type: "index" },
+  const columns: Column[] = [
     ...players.map(
       (player) =>
         ({
@@ -47,81 +54,90 @@ export function MatchTable({ match, games }: { match: Match; games: Game[] }) {
     ),
   ];
 
-  const gameRows: {
-    [playerId: (typeof players)[number]["id"]]: number;
-  }[] =
-    games?.map((game, index) =>
-      Object.fromEntries([
-        ["key", `game-${index}`],
-        ["index", index + 1],
-        ...game.scores.map((score) => [score.profileId, score.score]),
-      ]),
+  const gameRows: Row[] =
+    games?.map((game) =>
+      Object.fromEntries(
+        game.scores.map((score) => [score.profileId, score.score]),
+      ),
     ) ?? [];
 
-  return (
-    <div className="overflow-x-auto">
-      <Table
-        classNames={{
-          table: "sticky top-0",
-        }}
-        removeWrapper
-        aria-label="成績表"
-        bottomContent={<AddGameButton isDisabled={isPlayersShort} />}
-        isHeaderSticky
-      >
-        <TableHeader columns={playerColumns}>
-          {(column) => (
-            <TableColumn
-              key={column?.id}
-              className={classNames({
-                "px-1": column.type === "player" || column.type === "empty",
-                "w-4": column.type === "index",
-              })}
-            >
-              <div
-                className={classNames("relative", {
-                  "min-w-[70px]":
-                    column.type === "player" || column.type === "empty",
+  const totalPointsRow: Row = Object.fromEntries(
+    players.map((player) => [
+      player.id,
+      gameRows.reduce((acc, gameRow) => acc + gameRow[player.id], 0),
+    ]),
+  );
+
+  const test = true;
+  if (test) {
+    return (
+      <div className="overflow-x-auto">
+        <Table>
+          <TableHeader>
+            <TableColumn className="w-4">
+              <span />
+            </TableColumn>
+            {columns.map((column) => (
+              <TableColumn
+                key={column?.id}
+                className={classNames({
+                  "px-1": column.type === "player" || column.type === "empty",
+                  "w-4": column.type === "index",
                 })}
               >
-                <div className="absolute inset-0 flex items-center justify-center">
-                  {column.type === "empty" && <AddPlayerButton />}
-                  {column.type === "player" && (
-                    <span className="truncate">{column.name}</span>
-                  )}
+                <div
+                  className={classNames("relative", {
+                    "min-w-[70px]":
+                      column.type === "player" || column.type === "empty",
+                  })}
+                >
+                  <div className="absolute inset-0 flex items-center justify-center">
+                    {column.type === "empty" && <AddPlayerButton />}
+                    {column.type === "player" && (
+                      <span className="truncate">{column.name}</span>
+                    )}
+                  </div>
                 </div>
-              </div>
-            </TableColumn>
-          )}
-        </TableHeader>
-        <TableBody
-          emptyContent={
-            isPlayersShort
-              ? "参加者を追加してください"
-              : "まだデータはありません"
-          }
-          items={gameRows}
-        >
-          {(item) => (
-            <TableRow key={item.key}>
-              {(columnKey) => {
-                const value = getKeyValue(item, columnKey);
-                return (
+              </TableColumn>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {gameRows.map((item, index) => (
+              <TableRow key={index}>
+                <TableCell className="text-center text-default-500">
+                  {index + 1}
+                </TableCell>
+                {columns.map((column) => (
                   <TableCell
-                    key={columnKey}
+                    key={column.id}
                     className={classNames("text-center", {
-                      "text-danger": Number(value) < 0,
-                      "text-default-500": columnKey === "index",
+                      "text-danger": item[column.id] < 0,
+                      "text-default-500": column.type === "index",
                     })}
                   >
-                    {value}
+                    {item[column.id]}
                   </TableCell>
-                );
-              }}
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
-    </div>
-  );
+                ))}
+              </TableRow>
+            ))}
+            <tr>
+              <td colSpan={columns.length + 1}>
+                <AddGameButton isDisabled={isPlayersShort} />
+              </td>
+            </tr>
+          </TableBody>
+          <TableFooter>
+            <TableFooterRow>
+              <TableFooterCell className="text-center">合計</TableFooterCell>
+              {columns.map((column) => (
+                <TableFooterCell className="text-center" key={column.id}>
+                  {totalPointsRow[column.id]}
+                </TableFooterCell>
+              ))}
+            </TableFooterRow>
+          </TableFooter>
+        </Table>
+      </div>
+    );
+  }
 }
