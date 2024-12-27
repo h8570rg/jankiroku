8aceb9612631b27c459250fb4220429b08c55d6c
h8570rg <namao0627@gmail.com>
2024-12-28 00:20:26 +0900

    fix: update supabase

---
+++ app/(main)/(components)/ReleaseNotes/index.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import dynamic from "next/dynamic";
+
+const ReleaseNotesModal = dynamic(() => import("./ReleaseNotesModal"), {
+  ssr: false,
+});
+
+export function ReleaseNotes() {
+  return <ReleaseNotesModal />;
+}
