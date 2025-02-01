c93e0391e529160db405efcdc3f439432a4bf6fa
h8570rg <namao0627@gmail.com>
2025-02-01 16:06:43 +0900

    fix: lint-staged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,14 +1,3 @@
-const path = require("path");
-
-// TODO: エラーで使えないのでpackage.jsonのpre-commitを変更している。
-// see https://github.com/eslint/eslint/issues/19322
-
-const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(" --file ")}`;
-
 module.exports = {
-  "*.{js,jsx,ts,tsx}": [buildEslintCommand, "prettier --write"],
-  "*.css": ["prettier --write"],
+  "*.{js,jsx,ts,tsx,css,md}": ["prettier --write"],
 };
